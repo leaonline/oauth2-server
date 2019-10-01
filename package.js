262d2cffe17f87ddddb147021de70cfcaeb780cb
Jan Küster <jkuester@uni-bremen.de>
2019-10-01 17:53:31 +0200

    published 3.0.0

--- package.js
+++ package.js
@@ -2,13 +2,13 @@
 Package.describe({
   name: 'leaonline:oauth2-server',
   version: '3.0.0',
-  summary: 'OAuth 2 Server package',
+  summary: 'OAuth 2 Server (v3) with Meteor bindings',
   git: 'https://github.com/leaonline/oauth2-server.git'
 })
 
 Package.onUse(function (api) {
   api.versionsFrom('1.0')
-  api.use('ecmascript')
+  api.use('ecmascript@0.12.7')
   api.mainModule('oauth.js', 'server')
 })
 
