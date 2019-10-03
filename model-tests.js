4f858c4e51552e0951be9d9fd181affd932dee9e
jankapunkt <jkuester@uni-bremen.de>
2019-10-03 09:56:03 +0200

    model tests added

--- model-tests.js
+++ model-tests.js
@@ -2,7 +2,82 @@ import { assert } from 'meteor/practicalmeteor:chai'
 import { Model } from './model'
 
 describe('model', function () {
-  it('is not yet implemented', function () {
-    assert.fail()
+
+  describe('constructor', function () {
+    it ('is not yet implemented', function () {
+      assert.fail()
+    })
   })
+
+  describe('getAccessToken', function () {
+    it ('is not yet implemented', function () {
+      assert.fail()
+    })
+  })
+
+  describe('createClient', function () {
+    it ('is not yet implemented', function () {
+      assert.fail()
+    })
+  })
+
+  describe('getClient', function () {
+    it ('is not yet implemented', function () {
+      assert.fail()
+    })
+  })
+
+  describe('saveToken', function () {
+    it ('is not yet implemented', function () {
+      assert.fail()
+    })
+  })
+
+  describe('getAuthorizationCode', function () {
+    it ('is not yet implemented', function () {
+      assert.fail()
+    })
+  })
+
+  describe('saveAuthorizationCode', function () {
+    it ('is not yet implemented', function () {
+      assert.fail()
+    })
+  })
+
+  describe('revokeAuthorizationCode', function () {
+    it ('is not yet implemented', function () {
+      assert.fail()
+    })
+  })
+
+  describe('saveRefreshToken', function () {
+    it ('is not yet implemented', function () {
+      assert.fail()
+    })
+  })
+
+  describe('getRefreshToken', function () {
+    it ('is not yet implemented', function () {
+      assert.fail()
+    })
+  })
+
+  describe('grantTypeAllowed', function () {
+    it ('is not yet implemented', function () {
+      assert.fail()
+    })
+  })
+
+  /* optional:
+  generateAccessToken
+  generateAuthorizationCode
+  generateRefreshToken
+  getUser
+  getUserFromClient
+  grantTypeAllowed
+  revokeToken
+  validateScope
+  */
+
 })
