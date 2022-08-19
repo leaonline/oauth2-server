c34617a929ea8ae50e76ba6abbec5a10ec27c31d
ravitmg <tmgrawv@gmail.com>
2022-08-19 12:03:05 +0545

    update package name and github link

--- package.js
+++ package.js
@@ -1,9 +1,9 @@
 /* eslint-env meteor */
 Package.describe({
-  name: 'ravitmg:oauth2-server',
+  name: 'tmgrawv:oauth2-server',
   version: '4.2.1',
   summary: 'Node OAuth2 Server (v4) with Meteor bindings',
-  git: 'https://github.com/leaonline/oauth2-server.git'
+  git: 'https://github.com/ravitmg/oauth2-server.git'
 })
 
 Package.onUse(function (api) {
