871fd250d7f92d6a0f3117a96be9ec7fa3af8c1c
jankapunkt <jkuester@uni-bremen.de>
2024-11-01 09:55:42 +0100

    published 6.0.0-rc.1

--- package.js
+++ package.js
@@ -1,32 +1,33 @@
 /* eslint-env meteor */
 Package.describe({
   name: 'leaonline:oauth2-server',
-  version: '6.0.0-rc.0',
+  version: '6.0.0-rc.1',
   summary: 'Node OAuth2 Server (v4) with Meteor bindings',
   git: 'https://github.com/leaonline/oauth2-server.git'
 })
 
 Package.onUse(function (api) {
-  api.versionsFrom(['3.0.1'])
+  api.versionsFrom(['3.0'])
   api.use('ecmascript')
   api.mainModule('lib/oauth.js', 'server')
 })
 
 Npm.depends({
-  '@node-oauth/oauth2-server': '5.1.0',
-  'body-parser': '1.20.0'
+  '@node-oauth/oauth2-server': '5.2.0',
+  'body-parser': '1.20.3'
 })
 
 Package.onTest(function (api) {
   api.use([
-    'lmieulet:meteor-legacy-coverage@0.4.0',
-    'lmieulet:meteor-coverage@4.3.0',
-    'meteortesting:mocha@3.0.0-rc.1'
+    // FIXME: include, once we have a working coverage for Meteor 3
+    // 'lmieulet:meteor-legacy-coverage@0.4.0',
+    // 'lmieulet:meteor-coverage@4.3.0',
+    'meteortesting:mocha@3.2.0'
   ])
   api.use('ecmascript')
   api.use('mongo')
   api.use('jkuester:http@2.1.0')
-  api.use('dburles:mongo-collection-instances@1.0.0-rc300.1')
+  api.use('dburles:mongo-collection-instances@1.0.0')
   api.use('accounts-base')
   api.use('accounts-password')
 
